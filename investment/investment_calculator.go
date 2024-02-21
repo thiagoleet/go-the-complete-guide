8d7cfc2c470dc83760ef4632794a1c0845e20619
Thiago Leite <thiagofmleite@gmail.com>
2024-02-21 17:15:16 -0300

    New Project: Investment Calculator

---
+++ investment/investment_calculator.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	var investmentAmount = 1000.0
+	var expectedReturnRate = 5.5
+	var years = 10
+
+	var futureValue = investmentAmount * math.Pow((1+expectedReturnRate/100), float64(years))
+
+	fmt.Println(futureValue)
+
+}
