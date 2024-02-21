ba908737391bbd7c45e6ffd20de5daeab6d463a3
Thiago Leite <thiagofmleite@gmail.com>
2024-02-21 17:20:48 -0300

    Conversions and explicit types assignments

--- investment/investment_calculator.go
+++ investment/investment_calculator.go
@@ -6,8 +6,8 @@ import (
 )
 
 func main() {
-	var investmentAmount = 1000.0
-	var expectedReturnRate = 5.5
+	var investmentAmount float64 = 1000
+	var expectedReturnRate float64 = 5.5
 	var years = 10
 
 	var futureValue = investmentAmount * math.Pow((1+expectedReturnRate/100), float64(years))
