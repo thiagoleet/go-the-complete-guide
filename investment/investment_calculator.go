7c825e4b2bb23341b147a911c69917ba430169d0
Thiago Leite <thiagofmleite@gmail.com>
2024-02-21 20:40:12 -0300

    Improved user input interface

--- investment/investment_calculator.go
+++ investment/investment_calculator.go
@@ -7,12 +7,18 @@ import (
 
 func main() {
 	const inflationRate = 6.5
-	var investmentAmount float64 = 1000.0
-	var years = 10
-	expectedReturnRate := 5.5
+	var investmentAmount float64
+	var years int
+	var expectedReturnRate float64
 
-	fmt.Println("Enter investment amount: ")
+	fmt.Print("Enter investment amount: ")
 	fmt.Scan(&investmentAmount)
+
+	fmt.Print("Enter expected return rate: ")
+	fmt.Scan(&expectedReturnRate)
+
+	fmt.Print("Enter number of years: ")
+	fmt.Scan(&years)
 
 	futureValue := investmentAmount * math.Pow((1+expectedReturnRate/100), float64(years))
 	futureRealValue := futureValue / math.Pow(1+inflationRate/100, float64(years))
