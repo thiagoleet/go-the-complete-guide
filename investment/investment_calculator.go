8050a075b6dd2bec60227c54e3ad88ef51ce0c33
Thiago Leite <thiagofmleite@gmail.com>
2024-02-21 17:29:57 -0300

    Using pointer, user input and constants

--- investment/investment_calculator.go
+++ investment/investment_calculator.go
@@ -6,11 +6,18 @@ import (
 )
 
 func main() {
-	var investmentAmount, years = 1000.0, 10
+	const inflationRate = 6.5
+	var investmentAmount float64 = 1000.0
+	var years = 10
 	expectedReturnRate := 5.5
 
+	fmt.Println("Enter investment amount: ")
+	fmt.Scan(&investmentAmount)
+
 	futureValue := investmentAmount * math.Pow((1+expectedReturnRate/100), float64(years))
+	futureRealValue := futureValue / math.Pow(1+inflationRate/100, float64(years))
 
 	fmt.Println(futureValue)
+	fmt.Println(futureRealValue)
 
 }
