8283c7a41a51c6e8663fa2295641733d282d1233
Thiago Leite <thiagofmleite@gmail.com>
2024-02-21 21:57:36 -0300

    Using switch statement

--- essentials/bank/bank.go
+++ essentials/bank/bank.go
@@ -16,9 +16,46 @@ func main() {
 
 		fmt.Println("Your choice:", choice)
 
-		if choice == 1 {
+		// if choice == 1 {
+		// 	printBalance(accountBalance)
+		// } else if choice == 2 {
+		// 	fmt.Print("Your deposit: ")
+		// 	var depositAmmount float64
+		// 	fmt.Scan(&depositAmmount)
+
+		// 	if depositAmmount < 0 {
+		// 		fmt.Println("Invalid amount! Must be greater than zero")
+		// 		// return // stop the execution of the function
+		// 		continue
+		// 	}
+		// 	accountBalance += depositAmmount
+
+		// 	printBalance(accountBalance)
+		// } else if choice == 3 {
+		// 	fmt.Print("Withdraw amount: ")
+		// 	var withdrawAmmount float64
+		// 	fmt.Scan(&withdrawAmmount)
+
+		// 	if withdrawAmmount < 0 {
+		// 		fmt.Println("Invalid amount! Must be greater than zero")
+		// 		continue
+		// 	} else if withdrawAmmount > accountBalance {
+		// 		fmt.Println("Invalid amount! You can't withdraw more than you have!")
+		// 		continue
+		// 	}
+
+		// 	accountBalance -= withdrawAmmount
+
+		// 	printBalance(accountBalance)
+		// } else {
+		// 	fmt.Println("Goodbye!")
+		// 	break
+		// }
+
+		switch choice {
+		case 1:
 			printBalance(accountBalance)
-		} else if choice == 2 {
+		case 2:
 			fmt.Print("Your deposit: ")
 			var depositAmmount float64
 			fmt.Scan(&depositAmmount)
@@ -31,7 +68,7 @@ func main() {
 			accountBalance += depositAmmount
 
 			printBalance(accountBalance)
-		} else if choice == 3 {
+		case 3:
 			fmt.Print("Withdraw amount: ")
 			var withdrawAmmount float64
 			fmt.Scan(&withdrawAmmount)
@@ -47,13 +84,13 @@ func main() {
 			accountBalance -= withdrawAmmount
 
 			printBalance(accountBalance)
-		} else {
+		default:
 			fmt.Println("Goodbye!")
-			break
+			fmt.Println("Thanks for using Go Bank")
+
+			return
 		}
 	}
-
-	fmt.Println("Thanks for using Go Bank")
 
 }
 
