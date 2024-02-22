d32252d76011ef2097b95a5184636dee9c8e36a4
Thiago Leite <thiagofmleite@gmail.com>
2024-02-21 21:20:37 -0300

    Improving profit calculator with functions

--- essentials/profit/calculator.go
+++ essentials/profit/calculator.go
@@ -7,20 +7,44 @@ func main() {
 	var expenses float64
 	var taxRate float64
 
-	fmt.Print("Enter revenue: ")
-	fmt.Scan(&revenue)
+	// fmt.Print("Enter revenue: ")
+	// fmt.Scan(&revenue)
 
-	fmt.Print("Enter expenses: ")
-	fmt.Scan(&expenses)
+	revenue = getUserInput("Enter revenue: ")
 
-	fmt.Print("Enter tax rate: ")
-	fmt.Scan(&taxRate)
+	// fmt.Print("Enter expenses: ")
+	// fmt.Scan(&expenses)
 
-	ebt := revenue - expenses
-	profit := ebt * (1 - taxRate/100)
-	ratio := ebt / profit
+	expenses = getUserInput("Enter expenses: ")
 
-	fmt.Println(ebt)
-	fmt.Println(profit)
-	fmt.Println(ratio)
+	// fmt.Print("Enter tax rate: ")
+	// fmt.Scan(&taxRate)
+
+	taxRate = getUserInput("Enter tax rate: ")
+
+	// ebt := revenue - expenses
+	// profit := ebt * (1 - taxRate/100)
+	// ratio := ebt / profit
+
+	ebt, profit, ratio := calculateFinanctials(revenue, expenses, taxRate)
+
+	fmt.Printf("%.1f\n", ebt)
+	fmt.Printf("%.1f\n", profit)
+	fmt.Printf("%.3f\n", ratio)
+}
+
+func calculateFinanctials(revenue, expenses, taxRate float64) (ebt, profit, ratio float64) {
+	ebt = revenue - expenses
+	profit = ebt * (1 - taxRate/100)
+	ratio = ebt / profit
+
+	return
+}
+
+func getUserInput(text string) float64 {
+	var userInput float64
+	fmt.Print(text)
+	fmt.Scan(&userInput)
+
+	return userInput
 }
