1364448b215ea03fba6ff03edd0367a91b7e58d7
Thiago Leite <thiagofmleite@gmail.com>
2024-02-25 18:27:50 -0300

    Dynamic lists

--- lists/lists.go
+++ lists/lists.go
@@ -3,26 +3,33 @@ package main
 import "fmt"
 
 func main() {
-	var productNames [4]string
-	productNames = [4]string{"A Book"}
+	// var productNames [4]string
+	// productNames = [4]string{"A Book"}
 
-	prices := [4]float64{10.99, 9.99, 45.99, 20.0}
+	// prices := [4]float64{10.99, 9.99, 45.99, 20.0}
+
+	// fmt.Println(prices)
+	// fmt.Println(productNames)
+
+	// fmt.Println(prices[0])
+
+	// // Slices
+	// featuredPrice := prices[1:3]
+	// fmt.Println(featuredPrice)
+	// fmt.Println(len(featuredPrice), cap(featuredPrice))
+
+	// featuredPrice = prices[:3]
+	// fmt.Println(featuredPrice)
+	// fmt.Println(len(featuredPrice), cap(featuredPrice))
+
+	// featuredPrice = prices[1:]
+	// fmt.Println(len(featuredPrice), cap(featuredPrice))
+
+	prices := []float64{10.99, 9.99}
 
 	fmt.Println(prices)
-	fmt.Println(productNames)
 
-	fmt.Println(prices[0])
-
-	// Slices
-	featuredPrice := prices[1:3]
-	fmt.Println(featuredPrice)
-	fmt.Println(len(featuredPrice), cap(featuredPrice))
-
-	featuredPrice = prices[:3]
-	fmt.Println(featuredPrice)
-	fmt.Println(len(featuredPrice), cap(featuredPrice))
-
-	featuredPrice = prices[1:]
-	fmt.Println(len(featuredPrice), cap(featuredPrice))
+	prices = append(prices, 5.99)
+	fmt.Println(prices)
 
 }
