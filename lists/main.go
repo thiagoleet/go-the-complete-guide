13a766dd1f9f27f2aaf6ae08a55d2015f387de91
Thiago Leite <thiagofmleite@gmail.com>
2024-02-25 19:19:40 -0300

    Type Aliases

--- lists/main.go
+++ lists/main.go
@@ -2,6 +2,12 @@ package main
 
 import "fmt"
 
+type floatMap map[string]float64
+
+func (m floatMap) output() {
+	fmt.Println(m)
+}
+
 func main() {
 	userNames := make([]string, 2, 5)
 
@@ -10,12 +16,13 @@ func main() {
 
 	fmt.Println(userNames, len(userNames), cap(userNames))
 
-	courseRatings := make(map[string]float64, 3)
+	courseRatings := make(floatMap, 3)
 
 	courseRatings["go"] = 4.7
 	courseRatings["react"] = 4.8
 	courseRatings["angular"] = 4.9
 	courseRatings["node"] = 4.5
 
-	fmt.Println(courseRatings, len(courseRatings))
+	// fmt.Println(courseRatings, len(courseRatings))
+	courseRatings.output()
 }
