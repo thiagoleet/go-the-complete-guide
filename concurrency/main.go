b4d65541f8a889bac98e99eb51da95d2d7e665dc
Thiago Leite <thiagofmleite@gmail.com>
2024-02-26 18:20:33 -0300

    Using channels

--- concurrency/main.go
+++ concurrency/main.go
@@ -5,18 +5,36 @@ import (
 	"time"
 )
 
-func greet(phrase string) {
+func greet(phrase string, doneChan chan bool) {
 	fmt.Println("Hello!", phrase)
+
+	doneChan <- true
 }
 
-func slowGreet(phrase string) {
+func slowGreet(phrase string, doneChan chan bool) {
 	time.Sleep(3 * time.Second) // simulate a slow, long-taking task
 	fmt.Println("Hello!", phrase)
+
+	doneChan <- true
 }
 
 func main() {
-	go greet("Nice to meet you!")
-	go greet("How are you?")
-	go slowGreet("How ... are ... you ...?")
-	go greet("I hope you're liking the course!")
+	dones := make([]chan bool, 4)
+	// done := make(chan bool)
+
+	dones[0] = make(chan bool)
+	go greet("Nice to meet you!", dones[0])
+
+	dones[1] = make(chan bool)
+	go greet("How are you?", dones[1])
+
+	dones[2] = make(chan bool)
+	go slowGreet("How ... are ... you ...?", dones[2])
+
+	dones[3] = make(chan bool)
+	go greet("I hope you're liking the course!", dones[3])
+
+	for _, done := range dones {
+		<-done
+	}
 }
