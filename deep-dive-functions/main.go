eeeb95ed632d927c0a8fa5f03427d705d3579a1b
Thiago Leite <thiagofmleite@gmail.com>
2024-02-25 21:41:53 -0300

    Closures and Function Factory

--- deep-dive-functions/main.go
+++ deep-dive-functions/main.go
@@ -1,16 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
 	numbers := []int{1, 2, 3, 4}
+
+	double := createTramsformer(2)
+	triple := createTramsformer(3)
 
 	transformed := transformNumbers(&numbers, func(number int) int {
 		return number * 2
 	})
 
+	doubled := transformNumbers(&numbers, double)
+	tripled := transformNumbers(&numbers, triple)
+
 	fmt.Println(numbers)
 	fmt.Println(transformed)
+	fmt.Println(doubled)
+	fmt.Println(tripled)
 }
 
 func transformNumbers(numbers *[]int, transform func(int) int) []int {
@@ -20,4 +30,10 @@ func transformNumbers(numbers *[]int, transform func(int) int) []int {
 	}
 
 	return dNumbers
+}
+
+func createTramsformer(factor int) func(int) int {
+	return func(number int) int {
+		return number * factor
+	}
 }
