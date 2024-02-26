feb15c3023a505f00a7ad789fe404870a9c4f3bf
Thiago Leite <thiagofmleite@gmail.com>
2024-02-26 20:59:05 -0300

    Updating an event

--- rest-api/routes/events.go
+++ rest-api/routes/events.go
@@ -58,3 +58,38 @@ func createEvent(context *gin.Context) {
 
 	context.JSON(http.StatusCreated, gin.H{"message": "Event created successfully!", "event": event})
 }
+
+func updateEvent(context *gin.Context) {
+	eventId, err := strconv.ParseInt(context.Param("id"), 10, 64)
+
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": err.Error()})
+		return
+	}
+
+	_, err = models.GetEventById(eventId)
+
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": err.Error()})
+		return
+	}
+
+	var updateEvent models.Event
+	err = context.ShouldBindJSON(&updateEvent)
+
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": err.Error()})
+		return
+	}
+
+	updateEvent.ID = eventId
+	err = updateEvent.Update()
+
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": err.Error()})
+		return
+	}
+
+	context.JSON(http.StatusOK, gin.H{"message": "Event updated successfully!", "event": updateEvent})
+
+}
