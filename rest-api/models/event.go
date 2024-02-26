b21b47e0167f1784cb12d5273623d6eb9f479fad
Thiago Leite <thiagofmleite@gmail.com>
2024-02-26 20:29:20 -0300

    Inserting events

--- rest-api/models/event.go
+++ rest-api/models/event.go
@@ -1,21 +1,45 @@
 package models
 
-import "time"
+import (
+	"time"
+
+	"thiagoleite.me/go-event-booking-api/db"
+)
 
 type Event struct {
-	ID          int       `json:"id"`
+	ID          int64     `json:"id"`
 	Name        string    `json:"name" binding:"required"`
 	Description string    `json:"description" binding:"required"`
 	Location    string    `json:"location" binding:"required"`
 	DateTime    time.Time `json:"date_time" binding:"required"`
-	UserID      int       `json:"user_id"`
+	UserID      int64     `json:"user_id"`
 }
 
 var events = []Event{}
 
-func (e Event) Save() {
-	// TODO: add to database
-	events = append(events, e)
+func (e Event) Save() error {
+	query := `INSERT INTO events 
+	(name, description, location, dateTime, user_id)
+	VALUES (?, ?, ?, ?, ?)`
+	stmt, err := db.DB.Prepare(query)
+
+	if err != nil {
+		return err
+	}
+
+	defer stmt.Close()
+	result, err := stmt.Exec(e.Name, e.Description, e.Location, e.DateTime, e.UserID)
+
+	if err != nil {
+		return err
+	}
+
+	id, err := result.LastInsertId()
+
+	e.ID = (id)
+
+	return err
+
 }
 
 func GetAllEvents() []Event {
