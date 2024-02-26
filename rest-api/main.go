a1d91dc5a18b82cb78c0edb32d22c7e0c9c9d701
Thiago Leite <thiagofmleite@gmail.com>
2024-02-26 20:46:31 -0300

    Get Event By ID

--- rest-api/main.go
+++ rest-api/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 	"thiagoleite.me/go-event-booking-api/db"
@@ -13,13 +14,39 @@ func main() {
 
 	server := gin.Default()
 	server.GET("/events", getEvents)
+	server.GET("/events/:id", getEvent)
 	server.POST("/events", createEvent)
 	server.Run(":8080")
 }
 
 func getEvents(context *gin.Context) {
-	events := models.GetAllEvents()
+	events, err := models.GetAllEvents()
+
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": err.Error()})
+		return
+	}
+
 	context.JSON(http.StatusOK, events)
+}
+
+func getEvent(context *gin.Context) {
+	eventId, err := strconv.ParseInt(context.Param("id"), 10, 64)
+
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": err.Error()})
+		return
+	}
+
+	event, err := models.GetEventById(eventId)
+
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": err.Error()})
+		return
+	}
+
+	context.JSON(http.StatusOK, event)
+
 }
 
 func createEvent(context *gin.Context) {
@@ -31,8 +58,14 @@ func createEvent(context *gin.Context) {
 		return
 	}
 
-	event.ID = 1
 	event.UserID = 1
+
+	err = event.Save()
+
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": err.Error()})
+		return
+	}
 
 	context.JSON(http.StatusCreated, gin.H{"message": "Event created successfully!", "event": event})
 }
--- rest-api/models/event.go
+++ rest-api/models/event.go
@@ -15,8 +15,6 @@ type Event struct {
 	UserID      int64     `json:"user_id"`
 }
 
-var events = []Event{}
-
 func (e Event) Save() error {
 	query := `INSERT INTO events 
 	(name, description, location, dateTime, user_id)
@@ -36,13 +34,50 @@ func (e Event) Save() error {
 
 	id, err := result.LastInsertId()
 
-	e.ID = (id)
+	e.ID = id
 
 	return err
 
 }
 
-func GetAllEvents() []Event {
-	// TODO: get from database
-	return events
+func GetAllEvents() ([]Event, error) {
+	query := "SELECT * FROM events"
+	rows, err := db.DB.Query(query)
+
+	if err != nil {
+		return nil, err
+	}
+
+	defer rows.Close()
+
+	var events []Event
+
+	for rows.Next() {
+		var event Event
+		err := rows.Scan(&event.ID, &event.Name, &event.Description, &event.Location, &event.DateTime, &event.UserID)
+
+		if err != nil {
+			return nil, err
+		}
+
+		events = append(events, event)
+
+	}
+
+	return events, nil
+
+}
+
+func GetEventById(id int64) (*Event, error) {
+	query := "SELECT * FROM events WHERE id = ?"
+	row := db.DB.QueryRow(query, id)
+
+	var event Event
+	err := row.Scan(&event.ID, &event.Name, &event.Description, &event.Location, &event.DateTime, &event.UserID)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &event, nil
 }
