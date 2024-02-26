7326cf360a1647a49d99d13c90d8d94fa597a9e7
Thiago Leite <thiagofmleite@gmail.com>
2024-02-25 21:38:12 -0300

    Introduction to Anonymous Functions

--- deep-dive-functions/main.go
+++ deep-dive-functions/main.go
@@ -1,52 +1,23 @@
 package main
 
-import (
-	"fmt"
-)
-
-type transformFn func(int) int
+import "fmt"
 
 func main() {
 	numbers := []int{1, 2, 3, 4}
-	moreNumbers := []int{5, 1, 2}
-	doubled := transformNumbers(&numbers, double)
-	tripled := transformNumbers(&numbers, triple)
+
+	transformed := transformNumbers(&numbers, func(number int) int {
+		return number * 2
+	})
 
 	fmt.Println(numbers)
-	fmt.Println(doubled)
-	fmt.Println(tripled)
-
-	transformerFn1 := getTransformerFunction(&numbers)
-	transformerFn2 := getTransformerFunction(&moreNumbers)
-
-	transformedNumbers1 := transformNumbers(&numbers, transformerFn1)
-	transformedNumbers2 := transformNumbers(&moreNumbers, transformerFn2)
-
-	fmt.Println(transformedNumbers1)
-	fmt.Println(transformedNumbers2)
+	fmt.Println(transformed)
 }
 
-func transformNumbers(numbers *[]int, transform transformFn) []int {
+func transformNumbers(numbers *[]int, transform func(int) int) []int {
 	dNumbers := []int{}
 	for _, val := range *numbers {
 		dNumbers = append(dNumbers, transform(val))
 	}
 
 	return dNumbers
-}
-
-func getTransformerFunction(numbers *[]int) transformFn {
-	if (*numbers)[0] == 1 {
-		return double
-	} else {
-		return triple
-	}
-}
-
-func double(number int) int {
-	return number * 2
-}
-
-func triple(number int) int {
-	return number * 3
 }
---
+++ deep-dive-functions/transformedfunctions/transformedfunctions.go
@@ -0,0 +1,52 @@
+package transformedfunctions
+
+import (
+	"fmt"
+)
+
+type transformFn func(int) int
+
+func main() {
+	numbers := []int{1, 2, 3, 4}
+	moreNumbers := []int{5, 1, 2}
+	doubled := transformNumbers(&numbers, double)
+	tripled := transformNumbers(&numbers, triple)
+
+	fmt.Println(numbers)
+	fmt.Println(doubled)
+	fmt.Println(tripled)
+
+	transformerFn1 := getTransformerFunction(&numbers)
+	transformerFn2 := getTransformerFunction(&moreNumbers)
+
+	transformedNumbers1 := transformNumbers(&numbers, transformerFn1)
+	transformedNumbers2 := transformNumbers(&moreNumbers, transformerFn2)
+
+	fmt.Println(transformedNumbers1)
+	fmt.Println(transformedNumbers2)
+}
+
+func transformNumbers(numbers *[]int, transform transformFn) []int {
+	dNumbers := []int{}
+	for _, val := range *numbers {
+		dNumbers = append(dNumbers, transform(val))
+	}
+
+	return dNumbers
+}
+
+func getTransformerFunction(numbers *[]int) transformFn {
+	if (*numbers)[0] == 1 {
+		return double
+	} else {
+		return triple
+	}
+}
+
+func double(number int) int {
+	return number * 2
+}
+
+func triple(number int) int {
+	return number * 3
+}
