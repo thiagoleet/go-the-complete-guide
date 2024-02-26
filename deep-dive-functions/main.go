6ffda49ea2d354ee6b52f828a46474e7852bd310
Thiago Leite <thiagofmleite@gmail.com>
2024-02-25 21:33:07 -0300

    Use functions as return type

--- deep-dive-functions/main.go
+++ deep-dive-functions/main.go
@@ -8,12 +8,22 @@ type transformFn func(int) int
 
 func main() {
 	numbers := []int{1, 2, 3, 4}
+	moreNumbers := []int{5, 1, 2}
 	doubled := transformNumbers(&numbers, double)
 	tripled := transformNumbers(&numbers, triple)
 
 	fmt.Println(numbers)
 	fmt.Println(doubled)
 	fmt.Println(tripled)
+
+	transformerFn1 := getTransformerFunction(&numbers)
+	transformerFn2 := getTransformerFunction(&moreNumbers)
+
+	transformedNumbers1 := transformNumbers(&numbers, transformerFn1)
+	transformedNumbers2 := transformNumbers(&moreNumbers, transformerFn2)
+
+	fmt.Println(transformedNumbers1)
+	fmt.Println(transformedNumbers2)
 }
 
 func transformNumbers(numbers *[]int, transform transformFn) []int {
@@ -23,6 +33,14 @@ func transformNumbers(numbers *[]int, transform transformFn) []int {
 	}
 
 	return dNumbers
+}
+
+func getTransformerFunction(numbers *[]int) transformFn {
+	if (*numbers)[0] == 1 {
+		return double
+	} else {
+		return triple
+	}
 }
 
 func double(number int) int {
